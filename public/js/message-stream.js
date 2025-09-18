4929f03465e25d5f1011b6efbfd0140ac7cae79d
zoey <z@zoey.ninja>
2025-09-18 19:03:38 +0000

    moved js to it's own file

---
+++ public/js/message-stream.js
@@ -0,0 +1,19 @@
+import { marked } from 'https://cdn.jsdelivr.net/npm/marked/lib/marked.esm.js'
+
+document.body.addEventListener('htmx:sseClose', function (e) {
+  console.log('in message stream')
+  if (e.detail.type === 'message') {
+    const el = e.target // the element that was connected
+    const text = el.innerText
+
+    // Save to localStorage
+    localStorage.setItem(`chat-${el.id}`, text)
+
+    // Render Markdown
+    el.innerHTML = marked.parse(text)
+  }
+
+  // if (e.type === 'error') {
+  //   console.error('error', e)
+  // }
+})
