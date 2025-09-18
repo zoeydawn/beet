98a3456bfd2e79cee539db6027d45acaaaf9e862
zoey <z@zoey.ninja>
2025-09-18 19:22:42 +0000

    made it so the drawer closes when you click outside of it

--- public/js/utils.js
+++ public/js/utils.js
@@ -1,6 +1,18 @@
 const drawer = document.getElementById('drawer')
 const btn = document.getElementById('menu-btn')
 
-btn.addEventListener('click', () => {
+btn.addEventListener('click', (e) => {
+  e.stopPropagation() // prevent click bubbling
   drawer.classList.toggle('open')
+})
+
+// Close drawer if click happens outside
+document.addEventListener('click', (e) => {
+  if (
+    drawer.classList.contains('open') &&
+    !drawer.contains(e.target) &&
+    e.target !== btn
+  ) {
+    drawer.classList.remove('open')
+  }
 })
